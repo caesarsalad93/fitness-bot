90a6ad0f86b43a85dba743ee43589386d4fce4a9
Alexander Huynh <alexhuynher@gmail.com>
2024-10-03 15:34:51 -0700

    Initial commit

---
+++ src/bot.ts
@@ -0,0 +1,24 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+  ],
+});
+
+client.once('ready', () => {
+  console.log('Bot is ready!');
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    await message.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
