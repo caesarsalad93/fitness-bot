842c2ebc69fbeb5c40ce9662b41c6aabdd93873b
caesarsalad93 <alexhuynher@gmail.com>
2024-10-05 14:16:01 -0700

    initiate bot file

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,8 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import fs from 'fs'
+import path from 'path'
+import { Client, Collection, Events, GatewayIntentBits, Interaction, ChatInputCommandInteraction } from 'discord.js';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -9,16 +12,48 @@ const client = new Client({
   ],
 });
 
+client.commands = new Collection();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const commandsPath = path.join(__dirname, 'commands');
+
+async function loadCommands(): Promise<void> {
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const commandModule = await import(filePath);
+    const command = commandModule.default;
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
+}
+
+await loadCommands();
+
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
+client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-  if (message.content === '!ping') {
-    await message.reply('Pong!');
-  }
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`)
+    return;
+  } 
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+}
 });
 
 client.login(process.env.DISCORD_TOKEN);
