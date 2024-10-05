b256ce215cf1ac79de6b4d9db45d46470176f069
caesarsalad93 <alexhuynher@gmail.com>
2024-10-05 14:13:22 -0700

    add types file

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+import { ChatInputCommandInteraction, Client, Collection, CommandInteraction, SlashCommandBuilder } from 'discord.js';
+
+export interface Command {
+    data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+    execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+}
+
+declare module 'discord.js' {
+    export interface Client {
+        commands: Collection<string, Command>;
+    }
+}
