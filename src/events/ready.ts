9d6ca182cfbf0d434b480e6ca229034858018bc2
caesarsalad93 <alexhuynher@gmail.com>
2024-10-19 14:58:53 -0700

    Add comments to include test channel and live channel IDs

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -9,7 +9,10 @@ const ready = {
     console.log(`Ready! Logged in as ${client.user!.tag}`);
     // Schedule the weekly announcement
     // Replace 'YOUR_CHANNEL_ID' with the actual channel ID where you want to post the announcement
+    // Test channel: 1281049533073195070
+    // Live channel: 1011399499668803676
     scheduleWeeklyAnnouncement(client, "1011399499668803676");
+    
   },
 };
 
